f01524700235e981ceab1e5c53566fa597b29e18
Minty <129922465+mintyperez@users.noreply.github.com>
2023-06-03 12:18:31 -0700

    walking animation

--- Assets/Code/spritechanger.cs
+++ Assets/Code/spritechanger.cs
@@ -1,4 +1,3 @@
-//ORIGINAL SCRIPT BY YAE
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,234 +5,115 @@ using UnityEngine;
 public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    public Sprite newSprite1;
-    public Sprite newSprite2;
-    public Sprite newSprite3;
-    public Sprite newSprite4;
+    public Sprite[] upSprites;
+    public Sprite[] leftSprites;
+    public Sprite[] downSprites;
+    public Sprite[] rightSprites;
+    public Sprite[] upHoldingSprites;
+    public Sprite[] leftHoldingSprites;
+    public Sprite[] downHoldingSprites;
+    public Sprite[] rightHoldingSprites;
+    public Sprite[] upIdleSprites;
+    public Sprite[] leftIdleSprites;
+    public Sprite[] downIdleSprites;
+    public Sprite[] rightIdleSprites;
+    public float animationSpeed = 5f;
 
-    void Start() {
+    private int spriteIndex = 0;
+
+    void Start()
+    {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
-    void Update() {
-        
-        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
-            //timer with oscilating change sprite?
-            ChangeSprite(newSprite1);
+    void Update()
+    {
+        // Determine the current sprites based on the direction and holding state
+        Sprite[] currentSprites = GetSpritesByDirection(upSprites, leftSprites, downSprites, rightSprites);
+
+        // If no direction keys are pressed, set the idle sprites
+        if (currentSprites == null)
+        {
+            currentSprites = GetIdleSprites();
         }
 
-        
-        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) { 
-            ChangeSprite(newSprite2);
-        }
-        
-        
-        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) { 
-            ChangeSprite(newSprite3);
+        // Check if the character is holding an object
+        bool isHolding = CheckHolding();
+
+        // If holding an object, use the holding sprites
+        if (isHolding)
+        {
+            Sprite[] holdingSprites = GetSpritesByDirection(upHoldingSprites, leftHoldingSprites, downHoldingSprites, rightHoldingSprites);
+            if (holdingSprites != null && holdingSprites.Length > 0)
+            {
+                currentSprites = holdingSprites;
+            }
         }
 
-
-        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
-            ChangeSprite(newSprite4);
+        // Change the sprite based on the current sprites and sprite index
+        if (currentSprites != null && currentSprites.Length > 0)
+        {
+            spriteIndex = Mathf.FloorToInt(Time.time * animationSpeed) % currentSprites.Length;
+            ChangeSprite(currentSprites[spriteIndex]);
         }
     }
 
-    void ChangeSprite(Sprite newSprite) {
-        spriteRenderer.sprite = newSprite; 
+    void ChangeSprite(Sprite newSprite)
+    {
+        spriteRenderer.sprite = newSprite;
+    }
+
+    Sprite[] GetSpritesByDirection(Sprite[] up, Sprite[] left, Sprite[] down, Sprite[] right)
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            return up;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            return left;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            return down;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            return right;
+        }
+
+        return null;
+    }
+
+    Sprite[] GetIdleSprites()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            return upIdleSprites;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            return leftIdleSprites;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            return downIdleSprites;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            return rightIdleSprites;
+        }
+
+        return null;
+    }
+
+    bool CheckHolding()
+    {
+        // Add your logic here to check if the character is holding an object
+        // You can return true if the character is holding an object, false otherwise
+        // For example:
+        // return TrashEquip.IsHolding();
+
+        return false;
     }
 }
-
-// //SCRIPT THAT WORKED
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class SpriteChanger : MonoBehaviour
-// {
-//     public SpriteRenderer spriteRenderer;
-//     public Sprite[] upSprites;
-//     public Sprite[] leftSprites;
-//     public Sprite[] downSprites;
-//     public Sprite[] rightSprites;
-
-//     public float spriteChangeInterval = 0.2f; // Time interval between sprite changes
-//     private bool isWalking = false; // Indicates whether the character is walking
-//     private float timeSinceLastSpriteChange = 0f; // Time elapsed since the last sprite change
-//     private int spriteIndex = 0; // Index to keep track of the current sprite
-
-//     void Start()
-//     {
-//         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-//     }
-
-//     void Update()
-//     {
-//         // Check if any movement keys are pressed
-//         bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
-
-//         if (isMoving && !isWalking)
-//         {
-//             // Start walking animation
-//             isWalking = true;
-//             spriteIndex = 0;
-//             timeSinceLastSpriteChange = 0f;
-//         }
-//         else if (!isMoving && isWalking)
-//         {
-//             // Stop walking animation
-//             isWalking = false;
-//             ChangeSprite(null); // Set the default sprite when not walking
-//         }
-
-//         if (isWalking)
-//         {
-//             // Increment the time since the last sprite change
-//             timeSinceLastSpriteChange += Time.deltaTime;
-
-//             // Check if it's time to change the sprite
-//             if (timeSinceLastSpriteChange >= spriteChangeInterval)
-//             {
-//                 timeSinceLastSpriteChange = 0f;
-
-//                 // Determine the direction of movement based on the keys pressed
-//                 if (Input.GetKey(KeyCode.W))
-//                 {
-//                     // Move up
-//                     ChangeSprite(upSprites[spriteIndex]);
-//                 }
-//                 else if (Input.GetKey(KeyCode.A))
-//                 {
-//                     // Move left
-//                     ChangeSprite(leftSprites[spriteIndex]);
-//                 }
-//                 else if (Input.GetKey(KeyCode.S))
-//                 {
-//                     // Move down
-//                     ChangeSprite(downSprites[spriteIndex]);
-//                 }
-//                 else if (Input.GetKey(KeyCode.D))
-//                 {
-//                     // Move right
-//                     ChangeSprite(rightSprites[spriteIndex]);
-//                 }
-
-//                 // Increment the sprite index for the next iteration
-//                 spriteIndex = (spriteIndex + 1) % 2;
-//             }
-//         }
-//     }
-
-//     void ChangeSprite(Sprite newSprite)
-//     {
-//         spriteRenderer.sprite = newSprite;
-//     }
-// }
-
-
-// TEST
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class SpriteChanger : MonoBehaviour
-// {
-//     public SpriteRenderer spriteRenderer;
-//     public Sprite[] upSprites;
-//     public Sprite[] leftSprites;
-//     public Sprite[] downSprites;
-//     public Sprite[] rightSprites;
-//     public Sprite[] upHoldingSprites;
-//     public Sprite[] leftHoldingSprites;
-//     public Sprite[] downHoldingSprites;
-//     public Sprite[] rightHoldingSprites;
-
-//     public float spriteChangeInterval = 0.2f; // Time interval between sprite changes
-//     private bool isWalking = false; // Indicates whether the character is walking
-//     private bool isHoldingItem = false; // Indicates whether the character is holding an item
-//     private float timeSinceLastSpriteChange = 0f; // Time elapsed since the last sprite change
-//     private int spriteIndex = 0; // Index to keep track of the current sprite
-
-//     void Start()
-//     {
-//         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-//     }
-
-//     void Update()
-//     {
-//         // Check if any movement keys are pressed
-//         bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
-
-//         if (isMoving && !isWalking)
-//         {
-//             // Start walking animation
-//             isWalking = true;
-//             spriteIndex = 0;
-//             timeSinceLastSpriteChange = 0f;
-//         }
-//         else if (!isMoving && isWalking)
-//         {
-//             // Stop walking animation
-//             isWalking = false;
-//             ChangeSprite(null); // Set the default sprite when not walking
-//         }
-
-//         // Check if the player is holding an item
-//         isHoldingItem = GetComponentInChildren<PickUpPoint>() != null;
-
-//         if (isWalking)
-//         {
-//             // Increment the time since the last sprite change
-//             timeSinceLastSpriteChange += Time.deltaTime;
-
-//             // Determine the sprite arrays based on the holding state
-//             Sprite[] currentSprites;
-//             if (isHoldingItem)
-//             {
-//                 currentSprites = GetSpritesByDirectionAndState(upHoldingSprites, leftHoldingSprites, downHoldingSprites, rightHoldingSprites);
-//             }
-//             else
-//             {
-//                 currentSprites = GetSpritesByDirectionAndState(upSprites, leftSprites, downSprites, rightSprites);
-//             }
-
-//             // Check if it's time to change the sprite
-//             if (timeSinceLastSpriteChange >= spriteChangeInterval)
-//             {
-//                 timeSinceLastSpriteChange = 0f;
-
-//                 // Change the sprite based on the current direction and holding state
-//                 ChangeSprite(currentSprites[spriteIndex]);
-
-//                 // Increment the sprite index for the next iteration
-//                 spriteIndex = (spriteIndex + 1) % 2;
-//             }
-//         }
-//     }
-
-//     void ChangeSprite(Sprite newSprite)
-//     {
-//         spriteRenderer.sprite = newSprite;
-//     }
-
-//     Sprite[] GetSpritesByDirectionAndState(Sprite[] up, Sprite[] left, Sprite[] down, Sprite[] right)
-//     {
-//         if (Input.GetKey(KeyCode.W))
-//         {
-//             return up;
-//         }
-//         else if (Input.GetKey(KeyCode.A))
-//         {
-//             return left;
-//         }
-//         else if (Input.GetKey(KeyCode.S))
-//         {
-//             return down;
-//         }
-//         else if (Input.GetKey(KeyCode.D))
-//         {
-//             return right;
-//         }
-
-//         return null;
-//     }
-// }
