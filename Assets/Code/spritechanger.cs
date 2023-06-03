09c9f076e5473f1ff5c4eb1193ee74860fdcd4d5
Minty <129922465+mintyperez@users.noreply.github.com>
2023-06-02 19:35:29 -0700

    idk

--- Assets/Code/spritechanger.cs
+++ Assets/Code/spritechanger.cs
@@ -1,133 +1,133 @@
-// ORIGINAL SCRIPT BY YAE
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class spritechanger : MonoBehaviour
-// {
-//     public SpriteRenderer spriteRenderer;
-//     public Sprite newSprite1;
-//     public Sprite newSprite2;
-//     public Sprite newSprite3;
-//     public Sprite newSprite4;
-
-//     void Start() {
-//         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
-//     }
-
-//     void Update() {
-        
-//         if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
-//             //timer with oscilating change sprite?
-//             ChangeSprite(newSprite1);
-//         }
-
-        
-//         else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) { 
-//             ChangeSprite(newSprite2);
-//         }
-        
-        
-//         else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) { 
-//             ChangeSprite(newSprite3);
-//         }
-
-
-//         else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
-//             ChangeSprite(newSprite4);
-//         }
-//     }
-
-//     void ChangeSprite(Sprite newSprite) {
-//         spriteRenderer.sprite = newSprite; 
-//     }
-// }
-
-//SCRIPT THAT WORKED
+//ORIGINAL SCRIPT BY YAE
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpriteChanger : MonoBehaviour
+public class spritechanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
-    public Sprite[] upSprites;
-    public Sprite[] leftSprites;
-    public Sprite[] downSprites;
-    public Sprite[] rightSprites;
+    public Sprite newSprite1;
+    public Sprite newSprite2;
+    public Sprite newSprite3;
+    public Sprite newSprite4;
 
-    public float spriteChangeInterval = 0.2f; // Time interval between sprite changes
-    private bool isWalking = false; // Indicates whether the character is walking
-    private float timeSinceLastSpriteChange = 0f; // Time elapsed since the last sprite change
-    private int spriteIndex = 0; // Index to keep track of the current sprite
-
-    void Start()
-    {
+    void Start() {
         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
     }
 
-    void Update()
-    {
-        // Check if any movement keys are pressed
-        bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
-
-        if (isMoving && !isWalking)
-        {
-            // Start walking animation
-            isWalking = true;
-            spriteIndex = 0;
-            timeSinceLastSpriteChange = 0f;
-        }
-        else if (!isMoving && isWalking)
-        {
-            // Stop walking animation
-            isWalking = false;
-            ChangeSprite(null); // Set the default sprite when not walking
+    void Update() {
+        
+        if (Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) {
+            //timer with oscilating change sprite?
+            ChangeSprite(newSprite1);
         }
 
-        if (isWalking)
-        {
-            // Increment the time since the last sprite change
-            timeSinceLastSpriteChange += Time.deltaTime;
+        
+        else if (Input.GetKey(KeyCode.S) && !Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.D)) { 
+            ChangeSprite(newSprite2);
+        }
+        
+        
+        else if (Input.GetKey(KeyCode.A) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) { 
+            ChangeSprite(newSprite3);
+        }
 
-            // Check if it's time to change the sprite
-            if (timeSinceLastSpriteChange >= spriteChangeInterval)
-            {
-                timeSinceLastSpriteChange = 0f;
 
-                // Determine the direction of movement based on the keys pressed
-                if (Input.GetKey(KeyCode.W))
-                {
-                    // Move up
-                    ChangeSprite(upSprites[spriteIndex]);
-                }
-                else if (Input.GetKey(KeyCode.A))
-                {
-                    // Move left
-                    ChangeSprite(leftSprites[spriteIndex]);
-                }
-                else if (Input.GetKey(KeyCode.S))
-                {
-                    // Move down
-                    ChangeSprite(downSprites[spriteIndex]);
-                }
-                else if (Input.GetKey(KeyCode.D))
-                {
-                    // Move right
-                    ChangeSprite(rightSprites[spriteIndex]);
-                }
-
-                // Increment the sprite index for the next iteration
-                spriteIndex = (spriteIndex + 1) % 2;
-            }
+        else if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.W) && !Input.GetKey(KeyCode.S)) {
+            ChangeSprite(newSprite4);
         }
     }
 
-    void ChangeSprite(Sprite newSprite)
-    {
-        spriteRenderer.sprite = newSprite;
+    void ChangeSprite(Sprite newSprite) {
+        spriteRenderer.sprite = newSprite; 
     }
 }
+
+// //SCRIPT THAT WORKED
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class SpriteChanger : MonoBehaviour
+// {
+//     public SpriteRenderer spriteRenderer;
+//     public Sprite[] upSprites;
+//     public Sprite[] leftSprites;
+//     public Sprite[] downSprites;
+//     public Sprite[] rightSprites;
+
+//     public float spriteChangeInterval = 0.2f; // Time interval between sprite changes
+//     private bool isWalking = false; // Indicates whether the character is walking
+//     private float timeSinceLastSpriteChange = 0f; // Time elapsed since the last sprite change
+//     private int spriteIndex = 0; // Index to keep track of the current sprite
+
+//     void Start()
+//     {
+//         spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+//     }
+
+//     void Update()
+//     {
+//         // Check if any movement keys are pressed
+//         bool isMoving = Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D);
+
+//         if (isMoving && !isWalking)
+//         {
+//             // Start walking animation
+//             isWalking = true;
+//             spriteIndex = 0;
+//             timeSinceLastSpriteChange = 0f;
+//         }
+//         else if (!isMoving && isWalking)
+//         {
+//             // Stop walking animation
+//             isWalking = false;
+//             ChangeSprite(null); // Set the default sprite when not walking
+//         }
+
+//         if (isWalking)
+//         {
+//             // Increment the time since the last sprite change
+//             timeSinceLastSpriteChange += Time.deltaTime;
+
+//             // Check if it's time to change the sprite
+//             if (timeSinceLastSpriteChange >= spriteChangeInterval)
+//             {
+//                 timeSinceLastSpriteChange = 0f;
+
+//                 // Determine the direction of movement based on the keys pressed
+//                 if (Input.GetKey(KeyCode.W))
+//                 {
+//                     // Move up
+//                     ChangeSprite(upSprites[spriteIndex]);
+//                 }
+//                 else if (Input.GetKey(KeyCode.A))
+//                 {
+//                     // Move left
+//                     ChangeSprite(leftSprites[spriteIndex]);
+//                 }
+//                 else if (Input.GetKey(KeyCode.S))
+//                 {
+//                     // Move down
+//                     ChangeSprite(downSprites[spriteIndex]);
+//                 }
+//                 else if (Input.GetKey(KeyCode.D))
+//                 {
+//                     // Move right
+//                     ChangeSprite(rightSprites[spriteIndex]);
+//                 }
+
+//                 // Increment the sprite index for the next iteration
+//                 spriteIndex = (spriteIndex + 1) % 2;
+//             }
+//         }
+//     }
+
+//     void ChangeSprite(Sprite newSprite)
+//     {
+//         spriteRenderer.sprite = newSprite;
+//     }
+// }
 
 
 // TEST
