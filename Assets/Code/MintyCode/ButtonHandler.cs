54216477bb449a05f07f2c9ddb1190806cb186b5
Minty <129922465+mintyperez@users.noreply.github.com>
2023-06-07 13:03:34 -0700

    Coming soon

--- Assets/Code/MintyCode/ButtonHandler.cs
+++ Assets/Code/MintyCode/ButtonHandler.cs
@@ -1,13 +1,31 @@
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class ButtonHandler : MonoBehaviour
 {
     public CharacterSwitcher characterSwitcher;
     public int characterIndex;
     public string characterName;
+    public Button chooseButton;
+    public TextMeshProUGUI buttonText;
+    private string defaultButtonText;
 
     public void OnButtonClick()
     {
         characterSwitcher.SwitchCharacter(characterIndex, characterName);
+
+
+        if (characterIndex == 0)
+        {
+            buttonText.text = "Choose";
+            chooseButton.interactable = true;
+        }
+        else
+        {
+            buttonText.text = "Coming Soon";
+            chooseButton.interactable = false;
+        }
+        
     }
 }
--- Assets/Code/MintyCode/CharacterCycler.cs
+++ Assets/Code/MintyCode/CharacterCycler.cs
@@ -1,16 +1,21 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class CharacterCycler : MonoBehaviour
 {
     public CharacterSwitcher characterSwitcher;
     public GameObject[] characterImages;
     public string[] characterNames;
+    public Button chooseButton;
+    public TextMeshProUGUI buttonText;
 
     private int currentIndex = 0;
+    private string defaultButtonText;
 
     private void Start()
     {
+        defaultButtonText = buttonText.text;
         UpdateCharacter();
     }
 
@@ -35,5 +40,22 @@ public class CharacterCycler : MonoBehaviour
     private void UpdateCharacter()
     {
         characterSwitcher.SwitchCharacter(currentIndex, characterNames[currentIndex]);
+
+        if (characterNames[currentIndex] == "Theodore")
+        {
+            buttonText.text = defaultButtonText;
+            chooseButton.interactable = true;
+        }
+        else
+        {
+            buttonText.text = "Coming Soon";
+            chooseButton.interactable = false;
+        }
+    }
+
+    public void ChooseCharacter()
+    {
+        // Add your code here for what happens when the "Choose" button is clicked
+        Debug.Log("Character chosen: " + characterNames[currentIndex]);
     }
 }
