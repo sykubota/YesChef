83539068a73fdd63b9f56d78691e17ed7b4abf93
Minty <129922465+mintyperez@users.noreply.github.com>
2023-06-08 14:53:56 -0700

    Update randomize.cs

--- Assets/Code/MeganCode/randomize.cs
+++ Assets/Code/MeganCode/randomize.cs
@@ -1,25 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- using System;
- using System.Collections.Generic;
- using System.Security.Cryptography;
- using UnityEngine;
- using UnityEngine.UI;
 
 public class randomize : MonoBehaviour
 {
     public Sprite[] sprites;
 
-    // Start is called before the first frame update
     void Start()
     {
         GetComponent<SpriteRenderer>().sprite = sprites[UnityEngine.Random.Range(0, sprites.Length)];
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
